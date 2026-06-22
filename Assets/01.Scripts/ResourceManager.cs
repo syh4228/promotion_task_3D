f3abbad37b8e8b7a1956019f6de9b2545200d306
신영준 <kdrsa789@gmail.com>
2026-06-22 23:21:45 +0900

    feat :  ResourceManager 스크립트 추가
    어드레서블(Addressables)을 통해 에셋을 렉 없이 불러오고(Load), 중복 로딩을 막으며(Cache), 다 쓰면 지워버리는(Release) 리소스 전담 매니저

---
+++ Assets/01.Scripts/ResourceManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class ResourceManager : MonoBehaviour
+{
+    public static ResourceManager Instance { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private Dictionary<string, AsyncOperationHandle> _handles = new Dictionary<string, AsyncOperationHandle>();
+
+    public async UniTask<T> LoadAsset<T>(string address) where T : UnityEngine.Object
+    {
+        if (_handles.TryGetValue(address, out AsyncOperationHandle handle))
+        {
+            return handle.Result as T;
+        }
+
+        AsyncOperationHandle<T> loadHandle = Addressables.LoadAssetAsync<T>(address);
+
+        try
+        {
+            T result = await loadHandle.ToUniTask();
+
+            _handles[address] = loadHandle;
+            return result;
+        }
+        catch (System.Exception e) 
+        {
+            Debug.LogError($"에셋 로드 실패: {address} / Error: {e.Message}");
+
+            if (loadHandle.IsValid())
+                Addressables.Release(loadHandle);
+
+            return null;
+        }
+    }
+
+    public async UniTask<GameObject> InstantiateAsync(string address, Transform parent = null, bool instantiateInWorldSpace = false)
+    {
+        AsyncOperationHandle<GameObject> handle = Addressables.InstantiateAsync(address, parent, instantiateInWorldSpace);
+
+        try
+        {
+            GameObject instance = await handle.ToUniTask();
+
+            return instance;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"프리팹 생성 실패: {address} / Error: {e.Message}");
+
+            if (handle.IsValid())
+                Addressables.Release(handle);
+
+            return null;
+        }
+    }
+
+    public void LoadSprite(string address, System.Action<Sprite> callback)
+    {
+        if (_handles.TryGetValue(address, out AsyncOperationHandle handle))
+        {
+            callback?.Invoke(handle.Result as Sprite);
+            return;
+        }
+
+        AsyncOperationHandle<Sprite> handleOrigin = Addressables.LoadAssetAsync<Sprite>(address);
+
+        handleOrigin.Completed += (op) =>
+        {
+            if (op.Status == AsyncOperationStatus.Succeeded) 
+            {
+                _handles[address] = op; 
+                callback?.Invoke(op.Result);
+            }
+            else 
+            {
+                Debug.LogError($"스프라이트 로드 실패: {address}");
+            }
+        };
+    }
+    public async UniTask<Sprite> LoadSprite(string address)
+    {
+        if (_handles.TryGetValue(address, out AsyncOperationHandle handle))
+        {
+            return handle.Result as Sprite;
+        }
+
+        AsyncOperationHandle<Sprite> handleOrigin = Addressables.LoadAssetAsync<Sprite>(address);
+
+        try
+        {
+            Sprite result = await handleOrigin.ToUniTask();
+
+            _handles[address] = handleOrigin;
+
+            return result;
+        }
+        catch (System.Exception)
+        {
+            Debug.LogError($"스프라이트 로드 실패: {address}");
+
+            if (handleOrigin.IsValid())
+                Addressables.Release(handleOrigin);
+
+            return null;
+        }
+    }
+
+    public void Release(string address)
+    {
+        if (_handles.TryGetValue(address, out AsyncOperationHandle handle))
+        {
+            Addressables.Release(handle);
+            _handles.Remove(address);
+            Debug.Log($"에셋 메모리 해제 완료: {address}");
+        }
+    }
+}
