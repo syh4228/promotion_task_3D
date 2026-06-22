071013cc8be738e0c673cf011fcf0a8652b61d7a
신영준 <kdrsa789@gmail.com>
2026-06-22 22:26:45 +0900

    feat : 게임데이터매니저
    Json을 통해서 데이터를 가져오고 관리하는 게임데이터매니저

---
+++ Assets/01.Scripts/GameDataManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine.UIElements;
+
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager Instance { get; set; } // 실글턴 선언
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    [Serializable]
+    private class SerializationWrapper<T>
+    {
+        public List<T> item;
+    }
+
+    public Dictionary<string, ItemData> itemDataList {  get; private set; } = new Dictionary<string, ItemData> ();
+
+    private Dictionary<string, T> LoadData<T>(string tableName) where T : GameDataBase
+    {
+        string resourcePath = $"JsonOutput/{tableName}";
+
+        TextAsset textAsset = Resources.Load<TextAsset>(resourcePath);
+
+        if (textAsset == null)
+        {
+            Debug.LogError($"[Error] 리소스를 찾을 수 없습니다: Resources/{resourcePath}");
+            return new Dictionary<string, T>();
+        }
+
+        try
+        {
+            string jsonString = textAsset.text;
+
+            string wrappedJson = "{\"items\":" + jsonString + "}";
+            SerializationWrapper<T> wrapper = JsonUtility.FromJson<SerializationWrapper<T>>(wrappedJson);
+
+            if (wrapper != null && wrapper.items != null)
+            {
+                Debug.Log($"{typeof(T).Name} 데이터를 {wrapper.items.Count}개 로드했습니다.");
+                return wrapper.items.ToDictionary(item => item.Id.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"[{typeof(T).Name} JSON 로드 오류] {ex.Message}");
+        }
+
+        return new Dictionary<string, T>();
+    }
+
+    public void LoadItemData(string jsonPath)
+    {
+        itemDataList = LoadData<ItemData>(jsonPath);
+    }
+
+    public ItemData GetItemData(string id)
+    {
+        if (itemDataList == null || string.IsNullOrEmpty(id)) return null;
+
+        return itemDataList.TryGetValue(id, out var item) ? item : null;
+    }
+}
