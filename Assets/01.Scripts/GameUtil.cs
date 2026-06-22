394fe7935c9f983bf304465c62bb591d70d2468e
신영준 <kdrsa789@gmail.com>
2026-06-22 23:36:51 +0900

    feat :  GameUtil 스크립트
    JSON 로드, 아이콘 이미지 로드, 고유번호 발급을 관리하는 다용도 유틸리티 스크립트

---
+++ Assets/01.Scripts/GameUtil.cs
@@ -0,0 +1,41 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class GameUtil
+{
+    private static long _lastId = 0;
+
+    public static void LoadFullData()
+    {
+        GameDataManager.Instance.LoadItemData("Item");
+    }
+
+    public static async UniTask<Sprite> LoadAndSetSpriteImage(Image targetImage, string spritePath)
+    {
+        Sprite sprite = await ResourceManager.Instance.LoadSprite(spritePath);
+        if (sprite != null)
+        {
+            targetImage.sprite = sprite; 
+        }
+        return sprite;
+    }
+
+    public static long GenerateUniqueId()
+    {
+        long newId = DateTime.UtcNow.Ticks;
+
+        while (true)
+        {
+            long lastId = Volatile.Read(ref _lastId);
+            long idToAssign = (newId <= lastId) ? lastId + 1 : newId;
+
+            if (Interlocked.CompareExchange(ref _lastId, idToAssign, lastId) == lastId)
+            {
+                return idToAssign;
+            }
+        }
+    }
+}
