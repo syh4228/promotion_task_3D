49b99980f43b4cfa6558a0c657f8e98748047ccc
신영준 <kdrsa789@gmail.com>
2026-06-23 00:09:14 +0900

    feat : 게임데이터
    Json 파일 수정과 내용 추가에 맞춰 게임데이터 스크립트 내용추가
    아이템 아이콘 추가
    아이템 프리펩 추가

--- Assets/01.Scripts/GameData.cs
+++ Assets/01.Scripts/GameData.cs
@@ -13,9 +13,11 @@ public class ItemData : GameDataBase
     public string Name;
     public string Description;
     public string ItemType;
-    public string Grade;
+    public int Heal;
+    public float Speed;
     public int MaxStackCount;
     public int SellingPrice;
+    public int PurchasePrice;
     public string IconPath;
-
+    public string PrefabPath;
 }
