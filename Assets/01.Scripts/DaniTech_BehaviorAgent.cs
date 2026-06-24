fe61a4e2ab92b6e003dfed18b50593fe3e15ccb1
신영준 <kdrsa789@gmail.com>
2026-06-24 23:05:20 +0900

    feat : UI 버튼 컴포넌트

---
+++ Assets/01.Scripts/DaniTech_BehaviorAgent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Unity.Behavior;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class DaniTech_BehaviorAgent : MonoBehaviour
+{
+    [SerializeField] private BehaviorGraphAgent BehaviorAgent_Self;
+    [SerializeField] private List<GameObject> PatrolSpotGameObjectList;
+
+    private void OnEnable()
+    {
+        if (PatrolSpotGameObjectList != null && PatrolSpotGameObjectList.Count > 0)
+        {
+            BehaviorAgent_Self.SetVariableValue("PatrolSpotList", PatrolSpotGameObjectList);
+        }
+    }
+
+}
---
+++ Assets/01.Scripts/UIButton.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class UIButton : MonoBehaviour
+{
+    [SerializeField] private Button Button_Base;
+    [SerializeField] private Text Text_Base;
+    [SerializeField] private Image Image_Base;
+    [SerializeField] private Image Image_Select;
+
+    private bool _isSlotMenualUnbindEvent;
+
+    private void Awake()
+    {
+        InitUIButton();
+        SetDefaultUI();
+    }
+
+    private void OnDisable()
+    {
+        if (_isSlotMenualUnbindEvent == false)
+        {
+            Button_Base.onClick.RemoveAllListeners();
+        }
+    }
+
+
+    private void SetDefaultUI()
+    {
+        if (Image_Select != null)
+        {
+            Image_Select.gameObject.SetActive(false);
+        }
+    }
+
+    private void InitUIButton()
+    {
+        if (Button_Base != null)
+        {
+            return;
+        }
+
+        var button = this.gameObject.GetComponentInChildren<Button>();
+        if (button != null)
+        {
+            this.Button_Base = button;
+        }
+    }
+
+    public void BindOnClickButtonEvent(Action onClickCallback, bool isMenualUnbineEvent = false)
+    {
+        if (Button_Base == null) return;
+
+        Button_Base.onClick.AddListener(onClickCallback.Invoke);
+        _isSlotMenualUnbindEvent = isMenualUnbineEvent;
+    }
+
+    public void UnBindAllOnClickButtonEvent()
+    {
+        if (Button_Base == null) return;
+
+        Button_Base.onClick.RemoveAllListeners();
+    }
+
+    public void ChangeButtonText(string buttonStr)
+    {
+        if (Text_Base == null) return;
+
+        Text_Base.text = buttonStr;
+    }
+}
