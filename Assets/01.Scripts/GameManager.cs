60188018a4aa27283c4bfc9bd7caa62bce6ecb3c
신영준 <kdrsa789@gmail.com>
2026-06-26 22:18:22 +0900

    feat : 게임매니저 컴포넌트 작성
    1. 인벤토리 매니저와 게임데이터 매니저를 관리하는 게임매니저 컴포넌트 작성

--- Assets/01.Scripts/GameManager.cs
+++ Assets/01.Scripts/GameManager.cs
@@ -4,12 +4,25 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Inst { get; set; }
+    public static GameManager Inst { get; private set; }
 
+    public InventoryManager Inventory { get; private set; }
+    public GameDataManager Data { get; private set; }
 
     private void Awake()
     {
+        if (Inst != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
         Inst = this;
+        DontDestroyOnLoad(gameObject);
+
+        Data = gameObject.AddComponent<GameDataManager>();
+        Inventory = gameObject.AddComponent<InventoryManager>();
+
+        GameUtil.LoadFullData();
     }
 
 }
