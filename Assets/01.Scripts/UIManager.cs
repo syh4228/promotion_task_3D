2547ab2146b9664cf528776ee788fbedf5deecf0
신영준 <kdrsa789@gmail.com>
2026-06-26 22:21:40 +0900

    docs : UI매니저 수정
    1. 인벤토리 UI와 인벤토리 UI슬롯도 UI매니저를 통해 관리 되도록 수정

--- Assets/01.Scripts/UIManager.cs
+++ Assets/01.Scripts/UIManager.cs
@@ -9,9 +9,20 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Text _hpText;     // HP 텍스트 표기
     [SerializeField] private Text _potionText; // 포션 사용갯수 텍스트 표기
 
+    [Header("인벤토리 창 연결")]
+    [SerializeField] private GameObject _inventoryUIObject;
+
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        if (_inventoryUIObject != null)
+        {
+            _inventoryUIObject.SetActive(false);
+        }
     }
 
     // 체력 텍스트 표시 함수
@@ -30,5 +41,26 @@ public class UIManager : MonoBehaviour
         {
             _potionText.text = "사용갯수: " + potionCount;
         }
+    }
+    public void ToggleInventoryUI()
+    {
+        if (_inventoryUIObject != null)
+        {
+            bool isOpen = _inventoryUIObject.activeSelf;
+            _inventoryUIObject.SetActive(!isOpen);
+        }
+        else
+        {
+            Debug.LogWarning("[UIManager] 인벤토리 UI 오브젝트가 인스펙터에 연결되지 않았습니다!");
+        }
+    }
+
+    public InventoryUI GetInventoryUI()
+    {
+        if (_inventoryUIObject != null)
+        {
+            return _inventoryUIObject.GetComponent<InventoryUI>();
+        }
+        return null;
     }
 }
