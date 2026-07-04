b552eeb8c371a9bc5360da5da8c5c882a029469c
신영준 <kdrsa789@gmail.com>
2026-07-04 22:35:02 +0900

    feat : 상점NPC 컴포넌트, 상호작용 용 인터페이스 컴포넌트 추가
    1. 상호작용 용 인터페이스 컴포넌트 작성
    2. 상점 Npc 컴포넌트 작성
    2-1. UI매니저에서 상점UI 상호작용 힘수 호출

---
+++ Assets/01.Scripts/IInteractable.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+// 상호작용 인터페이스
+// 플레이어가 인터페이스만 보고 상호작용 실행
+public interface IInteractable
+{
+    void Interact();
+}
---
+++ Assets/01.Scripts/ShopNpc.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ShopNpc : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        if (UIManager.Instance != null)
+        {
+            UIManager.Instance.ToggleTradeUI();
+        }
+        else
+        {
+            Debug.LogWarning("[ShopNpc] UIManager 인스턴스를 찾을 수 없습니다.");
+        }
+    }
+}
